a718ae480c309afee61adc0f4fcae2a2a3dedf8c
jeseekashah <96967542+jeseekashah@users.noreply.github.com>
2022-02-27 09:03:46 +0530

    Add files via upload

---
+++ motion detection.py
@@ -0,0 +1,16 @@
+import cv2
+import numpy as np
+
+cap=cv2.VideoCapture(0)
+
+while cap.isOpened():
+    ret, frame=cap.read()
+
+    cv2.imshow("demo", frame)
+    if cv2.waitKey(40)==27:
+        break
+
+cv2.destroyAllWindows()
+cap.release()
+
+    
